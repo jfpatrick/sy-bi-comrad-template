59f8db95b3f4e7d5c5d4cad54644f24a727d1e61
Sara Zanzottera <sara.zanzottera@cern.ch>
2020-04-02 18:45:09 +0200

    Add banner

--- wizard.py
+++ wizard.py
@@ -1,7 +1,29 @@
+from os import system
+from time import sleep
 
 def main():
-    print("Here I am!!! :D")
+    system('clear')
+    print("""
+#################################################################################################
+  ___   ___     ___           ___    _     ___    __      __  _                        _ 
+ | _ ) |_ _|   | _ \  _  _   / _ \  | |_  | __|   \ \    / / (_)  ___  __ _   _ _   __| |
+ | _ \  | |    |  _/ | || | | (_) | |  _| |__ \    \ \/\/ /  | | |_ / / _` | | '_| / _` |
+ |___/ |___|   |_|    \_, |  \__\_\  \__| |___/     \_/\_/   |_| /__| \__,_| |_|   \__,_|
+                     |__/                                                               
 
+#################################################################################################
+""")
+    print("Welcome to the PyQt5 Project Setup Wizard!")
+    project_name = input("Please enter your projects' name:  ")
+    # Validate
+    print("A folder called '{} will be created as your project's root directory".format(project_name))
+    short_description = input("Please enter a short description of your project (~100 characters max):  ")
+    author_name = input("Please enter the project's author name:  ")
+    author_email = input("And his/her CERN email address:  ")
+    print("Thank you! Your project is being generated...")
+    # Generate project
+    sleep(1)
+    print("The project is ready. Please run 'charm {}' to open the project in the IDE".format(project_name))
 
 if __name__ == '__main__':
     main()
