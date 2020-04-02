9d7f819de0e1a7da1fd415a34bb6c2092df1dbef
Sara Zanzottera <sara.zanzottera@cern.ch>
2020-04-02 17:34:36 +0200

    setup.sh can launch wizard.py successfully

--- setup.sh
+++ setup.sh
@@ -7,9 +7,7 @@ source /acc/local/share/python/acc-py-pyqt/setup.sh
 acc-py venv venv
 source venv/bin/activate
 
-# Install template code and run wizard
-git clone --depth 1 --no-checkout https://gitlab.cern.ch/szanzott/be-bi-pyqt-template.git wizard
+# Checkout template code and run wizard
+git clone https://gitlab.cern.ch/szanzott/be-bi-pyqt-template.git wizard
 cd wizard
-git checkout master -- wizard.py
-
 python wizard.py
