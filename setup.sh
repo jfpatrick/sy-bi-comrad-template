fa8ceb4ecb31115e921eefdc0d65312b26a77574
Sara Zanzottera <sara.zanzottera@cern.ch>
2020-04-03 15:53:37 +0200

    Rough prototype of setup script does something

--- setup.sh
+++ setup.sh
@@ -1,17 +1,45 @@
 
+echo "_________________________________________________________________________"
+echo "       ___       __   __         ___    ___  __     ___       ___        "
+echo " |  | |__  |    /  ` /  \  |\/| |__      |  /  \     |  |__| |__         "
+echo " |/\| |___ |___ \__, \__/  |  | |___     |  \__/     |  |  | |___        "
+echo "     ____        ____  __     _       ___                      __        "
+echo "    / __ \__  __/ __ \/ /_   | |     / (_)___  ____ __________/ /        "
+echo "   / /_/ / / / / / / / __/   | | /| / / /_  / / __ `/ ___/ __  /         "
+echo "  / ____/ /_/ / /_/ / /_     | |/ |/ / / / /_/ /_/ / /  / /_/ /          "
+echo " /_/    \__, /\___\_\__/     |__/|__/_/ /___/\__,_/_/   \__,_/           "
+echo "       /____/                                                            "
+echo "_________________________________________________________________________"
+
 # Activate Acc-Py
+echo "=> Setting up temporary virtualenv"
 deactivate
 source /acc/local/share/python/acc-py-pyqt/setup.sh
 
 # Create local virtualenv
-acc-py venv venv
-source venv/bin/activate
+acc-py venv .temp-venv
+source .temp-venv/bin/activate
 
 # Checkout template code and run wizard
-git clone https://gitlab.cern.ch/szanzott/be-bi-pyqt-template.git wizard
-cd wizard
+# git clone https://gitlab.cern.ch/szanzott/be-bi-pyqt-template.git wizard
+# Temporary debugging replacement
+mkdir wizard
+cp -r ../be-bi-pyqt-template/!(venv) wizard
+
+cp wizard/wizard.py .
 python wizard.py
 
-# Move into the only available folder and install
+# Remove temporary venv and wizard.py
+echo "=> Removing temporary virtualenv"
+deactivate
+rm -rf .temp-venv
+rm wizard.py
+
+# Move into the only available folder, setup venv and install
 cd */
+acc-py venv venv
+source venv/bin/activate
 pip install -e .
+
+# remove yourself
+rm setup.sh
--- wizard.py
+++ wizard.py
@@ -1,47 +1,67 @@
 import os
 import shutil
-from time import sleep
+from pathlib import Path
 
 def main():
-    os.system('clear')
-    print("""
-================================================================================================
-  ___   ___     ___           ___    _     ___    __      __  _                        _ 
- | _ ) |_ _|   | _ \  _  _   / _ \  | |_  | __|   \ \    / / (_)  ___  __ _   _ _   __| |
- | _ \  | |    |  _/ | || | | (_) | |  _| |__ \    \ \/\/ /  | | |_ / / _` | | '_| / _` |
- |___/ |___|   |_|    \_, |  \__\_\  \__| |___/     \_/\_/   |_| /__| \__,_| |_|   \__,_|
-                     |__/                                                               
+    try:
+        os.system('clear')
+        print("""
+_________________________________________________________________________
+       ___       __   __         ___    ___  __     ___       ___    
+ |  | |__  |    /  ` /  \  |\/| |__      |  /  \     |  |__| |__     
+ |/\| |___ |___ \__, \__/  |  | |___     |  \__/     |  |  | |___                                                                       
+     ____        ____  __     _       ___                      __
+    / __ \__  __/ __ \/ /_   | |     / (_)___  ____ __________/ /
+   / /_/ / / / / / / / __/   | | /| / / /_  / / __ `/ ___/ __  / 
+  / ____/ /_/ / /_/ / /_     | |/ |/ / / / /_/ /_/ / /  / /_/ /  
+ /_/    \__, /\___\_\__/     |__/|__/_/ /___/\__,_/_/   \__,_/   
+       /____/                                                    
+_________________________________________________________________________
+    """)
+        print("Welcome to BI's PyQt5 Project Setup Wizard!")
+        project_name = input("Please enter your projects' name:  ")
+        # Validate
+        print("A folder called '{}' will be created as your project's root directory".format(project_name))
+        short_description = input("Please enter a short description of your project (~100 characters max):  ")
+        author_name = input("Please enter the project's author name:  ")
+        author_email = input("And his/her CERN email address:  ")
+        print("_________________________________________________________________________")
+        print("Your project is being generated...")
 
-================================================================================================
-""")
-    print("Welcome to the PyQt5 Project Setup Wizard!")
-    project_name = input("Please enter your projects' name:  ")
-    # Validate
-    print("A folder called '{} will be created as your project's root directory".format(project_name))
-    short_description = input("Please enter a short description of your project (~100 characters max):  ")
-    author_name = input("Please enter the project's author name:  ")
-    author_email = input("And his/her CERN email address:  ")
-    print("Thank you! Your project is being generated...")
+        # Generate project root
+        # os.mkdir(project_name)
 
-    # Generate project root
-    os.mkdir(project_name)
+        # Copy files from the template app
+        parent_path = Path(__file__)
+        project_path = parent_path.parent / project_name
+        print(" - Setting up folder structure under {}".format(project_path.absolute()))
+        os.rename("wizard", project_name)
+        # shutil.copytree(parent_path, project_path)
 
-    # Copy files from the template app
-    shutil.copytree("be_bi_pyqt_template", project_name)
+        # Change the root name in all files
+        for dname, dirs, files in os.walk(project_path):
+            for dirname in dirs:
+                if "be_bi_pyqt_template" in dirname:
+                    dpath = os.path.join(dname, dirname)
+                    os.rename(dpath, dpath.replace("be_bi_pyqt_template", project_name))
+            for fname in files:
+                # Temporary check, remove later
+                fpath = os.path.join(dname, fname)
+                if fname.split(".")[-1] in ["py", "ui", "qrc", "md", "gitignore", "yml", "sh"]:
+                    print(" - Processing {}".format(fpath))
+                    with open(fpath, "r") as f:
+                        s = f.read()
+                    s = s.replace("be_bi_pyqt_template", project_name)
+                    with open(fpath, "w") as f:
+                        f.write(s)
+                else:
+                    os.remove(fpath)
 
-    # Change the root name in all files
-    for dname, dirs, files in os.walk(project_name):
-        for fname in files:
-            fpath = os.path.join(dname, fname)
-            with open(fpath) as f:
-                s = f.read()
-            s = s.replace("be_bi_pyqt_template", project_name)
-            with open(fpath, "w") as f:
-                f.write(s)
+        print("The project is ready and it will now be pip installed in the local virtual environment.\n"
+              "Please run 'source activate.sh' and then 'charm {}' to open the project in the IDE.".format(project_name))
 
-    print("The project is read and it will now be pip installed in the local virtual environment."
-          "Please run 'source activate.sh' and then 'charm {}' to open the project in the IDE".format(project_name))
-
+    except KeyboardInterrupt:
+        print("\nStopping here. Bye!")
 
 
 if __name__ == '__main__':
