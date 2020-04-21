a607bb26e7900311e80c95475d55b42eed1719ca
Sara Zanzottera <sara.zanzottera@cern.ch>
2020-04-21 11:17:23 +0200

    Fix weird failure in tests

--- tests/test_example_application.py
+++ tests/test_example_application.py
@@ -4,12 +4,13 @@ from accwidgets.graph import ScrollingPlotWidget, CyclicPlotWidget
 from be_bi_pyqt_template.widgets.example_widget import ExampleWidget
 
 
-def test_can_open_main_window(monkeypatch):
+def test_can_open_main_window(monkeypatch, mock_pyjapc, qtbot):
     """
     This test showcases how to check whether specific Qt operations are performed during
     your test (in this case, opening a dialog).
     The ideas is to monkey-patch the interesting operation with a lambda that throws a
     recognizable exception, and then monitor the code for such exception.
+    NOTE: 'mock_pyjapc' and 'qtbot' are NOT unused parameters. They are needed to correctly perform the test.
     """
     # Define what to do if a QMessageBox tries to open
     def raise_exception_if_qmessagebox_opens():
