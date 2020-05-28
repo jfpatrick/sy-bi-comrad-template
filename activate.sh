f56b942954df4da20eb2f17f98688962c1f349f9
Sara Zanzottera <sara.zanzottera@cern.ch>
2020-05-28 11:05:27 +0200

    Fix .gitlab-ci.yml to workaround a pyqt5ac bug

--- activate.sh
+++ activate.sh
@@ -2,5 +2,8 @@ deactivate >/dev/null 2>&1
 source /acc/local/share/python/acc-py-pyqt/setup.sh
 source venv/bin/activate
 
-# Hook the accwidgets plugin for QtDesigner
+# Hook the accwidgets plugin for Qt Designer
 export PYQTDESIGNERPATH=$PWD/venv/lib/python3.6/site-packages/accwidgets/graph/designer
+
+# Put CO's PyCharm on the PATH
+export PATH=$PATH:/acc/local/share/python/pycharm/pycharm-community-2019.2.3/bin
