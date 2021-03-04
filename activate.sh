67a46a9f627fd630daa172912862adeba27a6293
Sara Zanzottera <sara.zanzottera@cern.ch>
2021-03-04 15:24:48 +0000

    Update activate.sh

--- activate.sh
+++ activate.sh
@@ -3,3 +3,9 @@
 # Activate the virtual environments
 source /acc/local/share/python/acc-py/base/pro/setup.sh
 source venv/bin/activate
+
+# Alias the shared Acc-Py PyCharm instance. 
+# NOTE: This PyCharm distro is kind of unofficial and might change!
+alias pycharm="/acc/local/share/python/pycharm/pycharm-community-2019.2.3/bin/pycharm.sh"
+# See https://wikis.cern.ch/display/ACCPY/PyCharm#PyCharm-Performanceissues
+#   if you encounter performance issues with PyCharm
