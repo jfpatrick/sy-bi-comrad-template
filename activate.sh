9c784893180e81c39eef79bf04751ecc3a4e1d7b
Sara Zanzottera <sara.zanzottera@cern.ch>
2021-03-18 09:24:21 +0000

    Update activate.sh

--- activate.sh
+++ activate.sh
@@ -4,8 +4,11 @@
 source /acc/local/share/python/acc-py/base/pro/setup.sh
 source venv/bin/activate
 
+# Alias the BI Python GUI Manager (https://gitlab.cern.ch/bisw-python/bipy-gui-manager)
+alias bipy-gui-manager="/user/bdisoft/operational/python/gui/bipy-gui-manager"
+
 # Alias the shared Acc-Py PyCharm instance. 
-# NOTE: This PyCharm distro is kind of unofficial and might change!
 alias pycharm="/acc/local/share/python/pycharm/pycharm-community-2019.2.3/bin/pycharm.sh"
+# NOTE: This PyCharm distro is kind of unofficial and might change!
 # See https://wikis.cern.ch/display/ACCPY/PyCharm#PyCharm-Performanceissues
-#   if you encounter performance issues with PyCharm
+# if you encounter performance issues with PyCharm
