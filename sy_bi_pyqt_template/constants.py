8e918169b0676add79769f94e801c224adb01e53
Sara Zanzottera <sara.zanzottera@cern.ch>
2021-06-21 15:33:05 +0200

    Create a basic ComRAD template, missing pyproject.toml

--- sy_bi_pyqt_template/constants.py
+++
@@ -1,8 +0,0 @@
-"""
-For reference, see:
-https://acc-py.web.cern.ch/gitlab/bisw-python/pyqt-tutorial/docs/stable/2-project-structure.html#project-name-constants-py
-"""
-
-APPLICATION_NAME = "SY BI PyQt Template"
-AUTHOR = "Sara Zanzottera"
-EMAIL = "sara.zanzottera@cern.ch"
--- sy_bi_pyqt_template/widgets/resources/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-For reference, see:
-https://acc-py.web.cern.ch/gitlab/bisw-python/pyqt-tutorial/docs/stable/2-project-structure.html#project-name-widgets-resources
-"""
